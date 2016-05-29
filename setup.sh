0a040c39705023770b39903a4873775b8692ef4f
Guilherme César dos Santos <gui.gui.guicesar12@hotmail.com>
2016-05-29 02:44:49 -0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+echo "Developed By Guilherme > fb.com/guilherme.cesar520"
+echo "--------------------------------------------------"
+echo "> [DEBUG] -)) Instalando Requisistos ........"
+sudo apt-get build-dep python-libtorrent
+sudo apt-get python-libtorrent
+echo "> [DEBUG] -)) Finalizando ......"
+sudo pip install feedparser
+echo "> [DEBUG] -)) Iniciando ....."
+echo "/////////////////////////////////////"
+echo "> Edite as configuracoes para mudar a serie ...."
+python PoshD.py
